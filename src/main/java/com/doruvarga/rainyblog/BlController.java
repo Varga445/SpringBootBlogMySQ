3ed6f4e6b7292881aa91f2915479dde719e87ed9
Varga445 <51417234+Varga445@users.noreply.github.com>
2020-06-14 13:45:29 +0300

    Update BlController.java

--- src/main/java/com/doruvarga/rainyblog/BlController.java
+++ src/main/java/com/doruvarga/rainyblog/BlController.java
@@ -36,10 +36,10 @@ public class BlController {
    
     @PostMapping("/blog")
     public Blog create(@RequestBody Map<String, String> body) {
-        //serviceProcesareBlog.processBlog(body);
+     
         String title = body.get("title");
         String content = body.get("content");
-        //String text=body.get("text");
+   
         return blogRespository.save(new Blog(title, content));
     }
 
@@ -47,11 +47,11 @@ public class BlController {
     @PutMapping("/blog/{id}")
     public Blog update(@PathVariable String id, @RequestBody Map<String, String> body) {
         int blogId = Integer.parseInt(id);
-        // getting blog
+    
         Blog blog = blogRespository.findOne(blogId);
         blog.setTitle(body.get("title"));
         blog.setContent(body.get("content"));
-        //blog.setText(body.get("text"));
+       
         return blogRespository.save(blog);
     }
 
