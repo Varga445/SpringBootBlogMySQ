d9d6ca39205ce65df262b80747bba158558ac989
Varga445 <51417234+Varga445@users.noreply.github.com>
2020-06-14 13:43:25 +0300

    Update BlRespository.java

--- src/main/java/com/doruvarga/rainyblog/BlRespository.java
+++ src/main/java/com/doruvarga/rainyblog/BlRespository.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Repository
 public interface BlRespository extends JpaRepository<Blog, Integer> {
 
-    // custom query to search to blog post by title or content
+    
     List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
 
 
